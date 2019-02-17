99b0b406343a91aa70acd2316eccceebcebe65be
Jabe Rooney <abrahamsahlzghi@gmail.com>
2019-02-17 17:52:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+#board is an array of strings
+# ["x","","","","0","","","","",]
+#index is a number to check
+#it could be 0,4,7,1
+def position_taken?(board, index)
+  position = board[index]
+  if position == "" || postion == " " || postion == nil
+    return false
+  else
+    return true
+end
